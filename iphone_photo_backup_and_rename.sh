ba552b047611b308c36e9ec57c55389902a78bda
charkster <31560568+charkster@users.noreply.github.com>
2021-01-30 16:19:18 -0700

    Add files via upload

--- iphone_photo_backup_and_rename.sh
+++ iphone_photo_backup_and_rename.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-DCIM_PATH="$(find /run/user/1000/gvfs/gphoto2:host* -maxdepth 2 -type d -name DCIM)"
-echo "Found ${DCIM_PATH}"
+idevicepair pair
+echo Hit Enter when iphone is paired:
+ifuse /media/iphone
+DCIM_PATH="/media/iphone/DCIM"
 echo Enter number of days to backup:
 read NUM_DAYS
-echo Enter directory to backup to:
-read BACKUP_DIR
-find "${DCIM_PATH}" -mtime -${NUM_DAYS} -exec gcp {} ${BACKUP_DIR} \;
-cd ${BACKUP_DIR}
-ls -al --time-style=full-iso | perl -ne 'if (/ ([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2})\.000000000 -0700 (IMG_[A-Z]?[0-9]{4}\.[a-zA-Z0-9]{3})/) { system("mv $7 $1_$2-$3_$4_$5_$6_$7\n"); }'
+find "${DCIM_PATH}" -mtime -${NUM_DAYS} -type f -exec gcp -p {} /destination \;
+cd /destination
+ls -al --time-style=full-iso | perl -ne 'if (/ ([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2})\.[0-9]* -0700 (IMG_[A-Z]?[0-9]{4}\.[a-zA-Z0-9]+)/) { system("mv $7 $1_$2-$3_$4_$5_$6_$7\n"); }'
